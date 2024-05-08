03391de694e3c1e0c81b48ad81a0319ce861ce63
Glawall <glawall217@gmail.com>
2024-05-08 11:46:09 +0100

    goes to a new page to view an article, adds vote button

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,10 +1,13 @@
+import {Link} from "react-router-dom"
+
 function ArticleCard({ article }) {
   return (
     <>
-      <h2 className="article-title">{article.title}</h2>
+    <h2>{article.title}</h2>
       <span className="article-info">
         <p> Written by: {article.author}</p>
         <p>Topic: {article.topic}</p>
+        <Link to={`/articles/${article.article_id}`}>See More Info Here</Link>
       </span>
       <span className="article-image">
         {article.article_img_url ? (
